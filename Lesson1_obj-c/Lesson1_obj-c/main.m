260717093c2ffdfce5b8e87e4901a5c2ec8e8e00
Vit_sh <vithits@yandex.ru>
2019-12-26 09:21:07 +0300

    ДЗ к уроку 3

--- Lesson1_obj-c/Lesson1_obj-c/main.m
+++ Lesson1_obj-c/Lesson1_obj-c/main.m
@@ -8,52 +8,99 @@
 
 #import <Foundation/Foundation.h>
 
-void checkChar() {
-    char charToCheck;
-    printf("Введите букву для проверки:");
-            scanf("%c", &charToCheck);
-    if (((charToCheck >= 'a') && (charToCheck <= 'z')) || ((charToCheck >= 'A') && (charToCheck <= 'Z'))) {
-        NSLog(@"Буква %c входит в английский алфавит", charToCheck);
-    } else {
-        NSLog(@"Данный сомвол не входит в английский алфавит");
-    }
-}
+// 2nd task
+enum CalcMethod {
+    Plus,
+    Minus,
+    Multiply,
+    DivideWhole,
+    GetRemainder
+};
 
-int addition(int a, int b) {
-    return a + b;
-}
+typedef enum CalcMethod CalcMethod;
 
-int multiplication(int a, int b) {
-    return a * b;
-}
-
-
-int calculate(NSString *method, int a, int b) {
+int calculate(CalcMethod method, int a, int b) {
     
-    if ([method isEqualToString:@"+"]) {
-        return a + b;
-    } else if ([method isEqualToString:@"-"]) {
-        return a - b;
-    } else if ([method isEqualToString:@"*"]) {
-        return a * b;
-    } else if ([method isEqualToString:@"/"]) {
-        return a / b;
-    } else if ([method isEqualToString:@"%"]) {
-        return a % b;
-    } else {
-        NSLog(@"Функция не знает переданный метод");
-        return 0;
+    switch (method) {
+        case Plus:
+            return a + b;
+            break;
+        case Minus:
+            return a - b;
+            break;
+        case Multiply:
+            return a * b;
+            break;
+        case DivideWhole:
+            return a / b;
+            break;
+        case GetRemainder:
+            return a % b;
+            break;
+        default:
+            return 0;
     }
 }
+
+// 3rd task
+enum Gender {
+    Male,
+    Female
+};
+
+typedef enum Gender Gender;
+
+// метод для получения текстового значения Gender
+NSString* genderText(Gender gender) {
+    NSString *result = nil;
+    switch (gender) {
+        case Male:
+            result = @"Male";
+            break;
+        case Female:
+            result = @"Female";
+            break;
+        default:
+            return 0;
+    }
+    return result;
+};
+
+struct Human {
+    NSString *name;
+    NSInteger age;
+    Gender gender;
+};
+
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
-        checkChar();
+        // 1st task
+        NSArray *planets = @[@"Earth", @"Mars", @"Jupiter"];
         
-        int sum = addition(3, 5);
-        int composition = multiplication(3, 5);
-        NSLog(@"Result: \n sum = %i, \n composition = %i", sum, composition);
+        for (NSNumber *planet in planets) {
+            NSLog(@"%@", planet);
+        }
+        
+        
+        // 2nd task
+        int sum = calculate(Plus, 5, 3);
+        int composition  = calculate(Multiply, 5, 3);
+        int remainder = calculate(GetRemainder, 5, 3);
+        
+        NSLog(@"Result: \n sum = %i, \n composition = %i, \n remainder = %i", sum, composition, remainder);
+        
+        
+        // 3rd task
+        struct Human man;
+        man.name = @"Jim Beam";
+        man.age = 35;
+        man.gender = Male;
+        
+        struct Human woman = {@"Sara Connor", 42, Female};
+        
+        NSLog(@"\nFirst human has name %@, age %li, gender %@. \nSecond human has name %@, age %li, gender %@.", man.name, (long)man.age, genderText(man.gender), woman.name, (long)woman.age, genderText(woman.gender));
     }
     return 0;
 }
