ca737baff0e1a4d521994ad150b7ad3594908892
Vit_sh <vithits@yandex.ru>
2020-01-15 16:02:43 +0300

    выполнено ДЗ к уроку 4

---
+++ Lesson4_Obj-C/Lesson4_Obj-C/Student.h
@@ -0,0 +1,17 @@
+//
+//  Student.h
+//  Lesson4_Obj-C
+//
+//  Created by Vitaly_Shishlyannikov on 15/01/2020.
+//  Copyright © 2020 Vitaly_Shishlayannikov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Student : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Lesson4_Obj-C/Lesson4_Obj-C/Student.m
@@ -0,0 +1,13 @@
+//
+//  Student.m
+//  Lesson4_Obj-C
+//
+//  Created by Vitaly_Shishlyannikov on 15/01/2020.
+//  Copyright © 2020 Vitaly_Shishlayannikov. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+@end
