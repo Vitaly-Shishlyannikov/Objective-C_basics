09295e621f61d4bb57c117b227189dcf552578e0
Vit_sh <vithits@yandex.ru>
2020-01-20 17:15:01 +0300

    базовый калькулятор

---
+++ L5_calculator/L5_calculator/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  L5_calculator
+//
+//  Created by Vitaly_Shishlyannikov on 20/01/2020.
+//  Copyright © 2020 Vitaly_Shishlayannikov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
