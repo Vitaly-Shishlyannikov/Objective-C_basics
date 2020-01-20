aad146101ca97ef332d959e01d5f55773e3acef0
Vit_sh <vithits@yandex.ru>
2020-01-20 17:15:25 +0300

    базовый калькулятор

--- L5_calculator/L5_calculator/main.m
+++ L5_calculator/L5_calculator/main.m
@@ -8,10 +8,51 @@
 
 #import <Foundation/Foundation.h>
 
+enum CalcMethod {
+    Plus,
+    Minus,
+    Multiply,
+    DivideWhole,
+    GetRemainder
+};
+
+typedef enum CalcMethod CalcMethod;
+
+int calculate(CalcMethod method, int a, int b) {
+    
+    switch (method) {
+        case Plus:
+            return a + b;
+            break;
+        case Minus:
+            return a - b;
+            break;
+        case Multiply:
+            return a * b;
+            break;
+        case DivideWhole:
+            return a / b;
+            break;
+        case GetRemainder:
+            return a % b;
+            break;
+        default:
+            return 0;
+    }
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        
+        // 2nd task
+        int sum = calculate(Plus, 5, 3);
+        int composition  = calculate(Multiply, 5, 3);
+        int remainder = calculate(GetRemainder, 5, 3);
+        
+        NSLog(@"Result: \n sum = %i, \n composition = %i, \n remainder = %i", sum, composition, remainder);
+        
     }
     return 0;
 }
+
