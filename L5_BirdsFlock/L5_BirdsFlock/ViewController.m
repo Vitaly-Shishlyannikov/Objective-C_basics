f5d13bc8659ee86bcf7ba3baade81ade26d6b298
Vit_sh <vithits@yandex.ru>
2020-01-22 12:50:25 +0300

    проект создан

---
+++ L5_BirdsFlock/L5_BirdsFlock/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  L5_BirdsFlock
+//
+//  Created by Vitaly_Shishlyannikov on 22/01/2020.
+//  Copyright © 2020 Vitaly_Shishlayannikov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ L5_BirdsFlock/L5_BirdsFlock/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  L5_BirdsFlock
+//
+//  Created by Vitaly_Shishlyannikov on 22/01/2020.
+//  Copyright © 2020 Vitaly_Shishlayannikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
