37f213d8021dfaad6f1ab1d7ca9d3eb94d7559af
Vit_sh <vithits@yandex.ru>
2020-02-10 09:52:39 +0300

    выполнено ДЗ к уроку 8

---
+++ L8_application_lifecycle/L8_application_lifecycle/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  L8_application_lifecycle
+//
+//  Created by Vitaly_Shishlyannikov on 10.02.2020.
+//  Copyright © 2020 Vitaly_Shishlayannikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ L8_application_lifecycle/L8_application_lifecycle/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  L8_application_lifecycle
+//
+//  Created by Vitaly_Shishlyannikov on 10.02.2020.
+//  Copyright © 2020 Vitaly_Shishlayannikov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
